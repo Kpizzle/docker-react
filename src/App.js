be41623614ccdc499f9059fd0d9d10b62a7f5221
Kyle <kp1289@gmail.com>
2019-11-11 20:03:21 +0000

    added intro message

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Test Automation CI/CD Project</p>
+        <hr></hr>
+        <p>
+          <div className='intro'>
+            Welcome to my CI/CD project!
+            <br />
+            Here is a sample application that I will be using to learn CI/CD
+            with a host with a cool hip tech stack!
+          </div>
+        </p>
         <a className='App-link' href='https://kylepotter.co.uk'>
           My Blog
         </a>
